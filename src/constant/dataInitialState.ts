8139c3ce81c6d96f8573598a70caceac5bc5314b
Harvey Yerik <yerik05vh@gmail.com>
2023-02-08 22:40:18 -0500

    Dev: update data json

---
+++ src/constant/dataInitialState.ts
@@ -0,0 +1,148 @@
+import { Welcome } from "../interface/data";
+
+export const DATA_INITIAL_STATE: Welcome = {
+  language: {
+    es: {
+      static: {
+        nav: {
+          home: "",
+          about: "",
+          portfolio: "",
+          language: "",
+          contact: "",
+          blog: "",
+        },
+        section_first: {
+          headline: "",
+          button_one: "",
+          button_two: "",
+        },
+        section_second: {
+          headline: "",
+        },
+        section_third: {
+          headline: "",
+          button: "",
+        },
+        section_four: {
+          headline: "",
+          text: "",
+          label_one: "",
+          label_two: "",
+          label_three: "",
+          button: "",
+        },
+        footer: {
+          text: "",
+          text_link: "",
+        },
+      },
+      dynamic: {
+        description: "",
+        portfolio: [
+          {
+            id: "",
+            name: "",
+            description: "",
+            image_path: "",
+            link_code: "",
+            link_preview: "",
+          },
+        ],
+      },
+    },
+    en: {
+      static: {
+        nav: {
+          home: "",
+          about: "",
+          portfolio: "",
+          language: "",
+          contact: "",
+          blog: "",
+        },
+        section_first: {
+          headline: "",
+          button_one: "",
+          button_two: "",
+        },
+        section_second: {
+          headline: "",
+        },
+        section_third: {
+          headline: "",
+          button: "",
+        },
+        section_four: {
+          headline: "",
+          text: "",
+          label_one: "",
+          label_two: "",
+          label_three: "",
+          button: "",
+        },
+        footer: {
+          text: "",
+          text_link: "",
+        },
+      },
+      dynamic: {
+        description: "",
+        portfolio: [
+          {
+            id: "",
+            name: "",
+            description: "",
+            image_path: "",
+            link_code: "",
+            link_preview: "",
+          },
+        ],
+      },
+    },
+  },
+
+  photo_url: "",
+  skills: [
+    "JavaScript",
+    "TypeScript",
+    "HTML",
+    "Python",
+    "Dart",
+    "C",
+    "Bash",
+    "Java",
+    "SQL",
+    "CSS",
+    "SASS",
+    "Pug",
+    "styled-components",
+    "TailwildCSS",
+    "Bootstrap",
+    "less",
+    "React",
+    "Vue.js",
+    "Vite",
+    "Flutter",
+    "Git",
+    "GitHub",
+  ],
+  social_media: {
+    linkedin: {
+      user: "",
+      link: "",
+    },
+    github: {
+      user: "",
+      link: "",
+    },
+    twitter: {
+      user: "",
+      link: "",
+    },
+    email: {
+      user: "",
+      link: "",
+    },
+  },
+};
---
+++ src/interface/data.ts
@@ -0,0 +1,89 @@
+export interface Welcome {
+  language: Language;
+  photo_url: string;
+  skills: string[];
+  social_media: SocialMedia;
+}
+
+export interface Language {
+  es: En;
+  en: En;
+}
+
+export interface En {
+  static: Static;
+  dynamic: Dynamic;
+}
+
+export interface Dynamic {
+  description: string;
+  portfolio: Portfolio[];
+}
+
+export interface Portfolio {
+  id: string;
+  name: string;
+  description: string;
+  image_path: string;
+  link_code: string;
+  link_preview: string;
+}
+
+export interface Static {
+  nav: Nav;
+  section_first: SectionFirst;
+  section_second: SectionSecond;
+  section_third: SectionThird;
+  section_four: SectionFour;
+  footer: Footer;
+}
+
+export interface Footer {
+  text: string;
+  text_link: string;
+}
+
+export interface Nav {
+  home: string;
+  about: string;
+  portfolio: string;
+  language: string;
+  contact: string;
+  blog: string;
+}
+
+export interface SectionFirst {
+  headline: string;
+  button_one: string;
+  button_two: string;
+}
+
+export interface SectionFour {
+  headline: string;
+  text: string;
+  label_one: string;
+  label_two: string;
+  label_three: string;
+  button: string;
+}
+
+export interface SectionSecond {
+  headline: string;
+}
+
+export interface SectionThird {
+  headline: string;
+  button: string;
+}
+
+export interface SocialMedia {
+  linkedin: Email;
+  github: Email;
+  twitter: Email;
+  email: Email;
+}
+
+export interface Email {
+  user: string;
+  link: string;
+}
