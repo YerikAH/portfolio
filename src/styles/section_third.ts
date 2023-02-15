64898a1be783566ad72dec483b69e6c9bb5eac4e
Harvey Yerik <yerik05vh@gmail.com>
2023-02-14 22:58:58 -0500

    Dev: create section project

--- src/styles/section_third.ts
+++ src/styles/section_third.ts
@@ -21,6 +21,69 @@ export const SubtitleOne = styled.h2`
   margin-top: 1.25rem;
 `;
 
-export const BoxGrid = styled.div``;
+export const BoxGrid = styled.div`
+  max-width: 1200px;
+  width: 100%;
+  display: grid;
+  margin: 0 auto;
+  place-items: center;
+  padding: 0 2rem;
+  margin-top: 3.625rem;
+  @media (min-width: 720px) {
+    grid-template-columns: repeat(2, 45%);
+    gap: 5%;
+  }
+`;
 
 export const ProjectStyle = styled.div``;
+
+// no variables css and no rem
+export const DecorationBox = styled.div`
+  background: rgba(243, 243, 243, 0.1);
+  border: 0.5px solid #dddddd;
+  backdrop-filter: blur(12.5px);
+  border-radius: 15px;
+  position: relative;
+`;
+
+export const AssetImage = styled.img`
+  position: absolute;
+  max-width: 400px;
+  max-height: 250px;
+  width: 100%;
+  height: 100%;
+  border-radius: 15px;
+`;
+
+export const BoxInfo = styled.div`
+  margin-top: 18px;
+`;
+
+export const TitleProject = styled.h4`
+  font: normal normal 400 1rem/1.5rem var(--font-lato);
+  text-align: justify;
+  letter-spacing: 0.02em;
+  color: var(--black);
+`;
+
+export const TextProject = styled.p`
+  font: normal normal 400 12px/1.25rem var(--font-lato);
+  text-align: justify;
+  letter-spacing: 0.02em;
+  color: var(--black);
+  opacity: 0.6;
+`;
+
+export const BoxButtons = styled.div`
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+`;
+
+export const TextButton = styled.button`
+  border-radius: 5px;
+  color: var(--black);
+  font: normal normal 400 10px/10px var(--font-lato);
+  border: none;
+  background: transparent;
+`;
