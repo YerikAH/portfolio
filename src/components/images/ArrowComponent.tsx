941329266a42838fa8a3c70d7cfe48c39b9045a6
Harvey Yerik <yerik05vh@gmail.com>
2023-02-21 13:11:40 -0500

    Dev: add icon arrow dar

--- src/components/images/ArrowComponent.tsx
+++ src/components/images/ArrowComponent.tsx
@@ -1,23 +1,28 @@
+import { useContext } from "react";
+import ThemeContext from "../../context/themeContext";
 import { SvgGeneral } from "../../styles/images/icons";
 
 export default function ArrowComponent() {
+  const themeContext = useContext(ThemeContext);
   return (
-    <SvgGeneral
-      xmlns="http://www.w3.org/2000/svg"
-      className="icon icon-tabler icon-tabler-arrow-narrow-right"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      strokeWidth="2"
-      stroke="#FFF"
-      fill="none"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-      <path d="M5 12l14 0"></path>
-      <path d="M15 16l4 -4"></path>
-      <path d="M15 8l4 4"></path>
-    </SvgGeneral>
+    <ThemeContext.Provider value={themeContext}>
+      <SvgGeneral
+        xmlns="http://www.w3.org/2000/svg"
+        className={themeContext.theme}
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        strokeWidth="2"
+        stroke="#FFF"
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <path d="M5 12l14 0"></path>
+        <path d="M15 16l4 -4"></path>
+        <path d="M15 8l4 4"></path>
+      </SvgGeneral>
+    </ThemeContext.Provider>
   );
 }
