93e25b1be336f9b7032743de1f5bccbd339e9175
Harvey Yerik <yerik05vh@gmail.com>
2023-02-21 12:00:10 -0500

    Dev: add styles blog

--- src/components/pages/blog/section/SectionFirstBlog.tsx
+++ src/components/pages/blog/section/SectionFirstBlog.tsx
@@ -1,7 +1,51 @@
-import React from 'react'
+import {
+  BoxGridBlog,
+  BoxGridOne,
+  BoxGridTwo,
+  DecorationBox,
+  HeadlinePrincipal,
+  NavigationBox,
+  NavigationGeneral,
+  NavigationSubtitleButton,
+  NavigationTitle,
+  NavigationTitleButton,
+  SectionStylesBlog,
+  Separate,
+  SubtitleOne,
+  SubtitleTwo,
+} from "../../../../styles/blog/section_first_blog";
 
 export default function SectionFirstBlog() {
   return (
-    <div>SectionFirstBlog</div>
-  )
+    <SectionStylesBlog>
+      <BoxGridBlog>
+        <BoxGridOne>
+          <NavigationBox>
+            <NavigationTitleButton>Blog</NavigationTitleButton>
+          </NavigationBox>
+          <NavigationGeneral>
+            <Separate />
+            <NavigationBox>
+              <NavigationTitle>Topics</NavigationTitle>
+              <NavigationSubtitleButton>Experiencias</NavigationSubtitleButton>
+              <NavigationSubtitleButton>
+                Reseñas de libros
+              </NavigationSubtitleButton>
+              <NavigationSubtitleButton>Programación</NavigationSubtitleButton>
+            </NavigationBox>
+            <Separate />
+          </NavigationGeneral>
+        </BoxGridOne>
+        <BoxGridTwo>
+          <DecorationBox>
+            <HeadlinePrincipal>Blog</HeadlinePrincipal>
+            <SubtitleOne>
+              Historias de un desarrollador web: reflexiones y experiencias
+            </SubtitleOne>
+          </DecorationBox>
+          <SubtitleTwo>Ultimos blogs</SubtitleTwo>
+        </BoxGridTwo>
+      </BoxGridBlog>
+    </SectionStylesBlog>
+  );
 }
--- src/styles/blog/section_first_blog.ts
+++ src/styles/blog/section_first_blog.ts
@@ -5,13 +5,20 @@ export const SectionStylesBlog = styled.section`
   display: flex;
   justify-content: center;
   align-items: center;
+  padding-top: 8.5rem;
+  @media (min-width: 720px) {
+    padding-top: 10.9875rem;
+  }
+
 `;
 export const BoxGridBlog = styled.div`
   max-width: 1100px;
   width: 100%;
   display: grid;
+  gap: 8%;
+  padding: 0 1rem;
   @media (min-width: 720px) {
-    grid-template-columns: 30% 70%;
+    grid-template-columns: 26% 66%;
   }
 `;
 export const BoxGridOne = styled.div``;
@@ -19,37 +26,95 @@ export const BoxGridTwo = styled.div``;
 
 export const DecorationBox = styled.div`  
   border-radius: 1rem;
-  padding: 2rem 2.5rem 2rem 4rem;
+  padding: 2rem ;
   background: linear-gradient(93.89deg, #2472CE 15.64%, #2449CE 90.51%);
   width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: start;
+  position: relative;
+  flex-direction: column;
+  @media (min-widh: 720px) {
+    padding: 2rem 2.5rem 2rem 4rem;
+  }
 `;
 export const HeadlinePrincipal = styled.h1`
   color: var(--white);
-  font: normal normal 700 2rem/1rem var(--font-lato)
+  margin: 0;
+  font: normal normal 700 2rem/2.5rem var(--font-lato)
 `
 export const SubtitleOne = styled.h4`
   color: var(--white);
-  font: normal normal 500 1rem/1rem var(--font-lato)
+  font: normal normal 500 1rem/1.2rem var(--font-lato)
 `
 export const DecorationImage = styled.img``
 
-export const SubtitleTwo = styled.h2``
-export const GroupBox = styled.div``
+export const SubtitleTwo = styled.h2`
+  color: var(--black);
+  font: normal normal 700 2rem/2.5rem var(--font-lato)
+
+`
+export const GroupBox = styled.div`
+  display: grid;
+`
+export const GroupContent = styled.div``
 export const SpanText = styled.span``
 export const TitleBox = styled.h6``
 export const TextBox = styled.p``
 
 
 
-export const NavigationBox = styled.nav``
+export const NavigationBox = styled.div`
+display: flex;
+flex-direction: column;
+margin: 1rem 0;
+`
 
-export const NavigationGroupList = styled.ul``
 
-export const NavigationTitleButton = styled.button
+export const NavigationGeneral = styled.div`
+  height: 0;
+  overflow: hidden;
+  @media (min-widh: 720px) {
+    height: auto;
+    overflow: auto;
+  }
+`
+export const NavigationTitleButton = styled.button`
+  background-color: var(--black-10);
+  border-radius: 8px;
+  padding:  1rem;
+  color: var(--blue);
+  font: normal normal 700 1.1rem/1.2rem var(--font-lato);
+  text-align: left;
+`
 
-export const NavigationTitle = styled.h3``
-export const NavigationSubtitleButton = styled.button``
+export const NavigationTitle = styled.h3`
+color: var(--black);
+  font: normal normal 600 1.2rem/1.2rem var(--font-lato);
+  text-align: left;
+  padding: 1rem;
 
-export const Separate = styled.div``
+`
+export const NavigationSubtitleButton = styled.button`
+
+  background-color: transparent;
+  border-radius: 8px;
+  padding: 1rem;
+  color: var(--black-pale);
+  font: normal normal 400 1rem/1.2rem var(--font-lato);
+  text-align: left;
+  opacity: 0.8;
+  &:hover{
+    background-color: var(--black-10);
+  }
+
+`
+
+export const Separate = styled.div`
+  width: 100%;
+  height: 0.5px;
+  opacity: 0.5;
+  background-color: var(--black);
+`
 
 export const TextDecoration = styled.p``
