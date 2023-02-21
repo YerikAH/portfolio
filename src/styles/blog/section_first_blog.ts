d357bec10beb64c831a3a40de3144c40d7067ae6
Harvey Yerik <yerik05vh@gmail.com>
2023-02-20 21:15:36 -0500

    Dev: update project

---
+++ src/styles/blog/section_first_blog.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const SectionStylesBlog = styled.section`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+export const BoxGridBlog = styled.div`
+  max-width: 1100px;
+  width: 100%;
+  display: grid;
+`;
+export const BoxGridOne = styled.div``;
+export const BoxGridTwo = styled.div``;
