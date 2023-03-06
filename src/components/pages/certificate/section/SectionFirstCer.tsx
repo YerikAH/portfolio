34afb0da371553cac26deb72df07cd9d8b818176
Harvey Yerik <yerik05vh@gmail.com>
2023-03-05 22:21:45 -0500

    Dev: update project

--- src/components/pages/certificate/section/SectionFirstCer.tsx
+++ src/components/pages/certificate/section/SectionFirstCer.tsx
@@ -36,19 +36,17 @@ export default function SectionFirstCer() {
           </BoxInfo>
         </BoxCircle>
         <BoxContain>
-          <BoxCertificate>
-            <TitleCertificate>Certificado de HTML y CSS</TitleCertificate>
-            <TextCertificate>
-              En el curso aprendi a conceptos avanzados en HTML y CSS, con los conocimientos
-              adquiridos pude desarrollar páginas web responsivas y como unos estilos más avanzados
-              como animaciones y efectos visuales.
-            </TextCertificate>
-            <BackgroundCertificate>
-              <ImageCertificate src='https://static.platzi.com/media/user_upload/diploma-excel_page-0001-65606338-9a09-46e5-ad27-150a8329186a.jpg' />
-              <ImageLogoCertificate src='https://static.platzi.com/static/images/footer/logo.png' />
-            </BackgroundCertificate>
-            <ButtonCertificate>Detalles del certificado</ButtonCertificate>
-          </BoxCertificate>
+          {dataContext.certificate.certificates.map((item) => (
+            <BoxCertificate>
+              <TitleCertificate>{item.name}</TitleCertificate>
+              <TextCertificate>{item.description}</TextCertificate>
+              <BackgroundCertificate>
+                <ImageCertificate src={item.image} />
+                <ImageLogoCertificate src='https://static.platzi.com/static/images/footer/logo.png' />
+              </BackgroundCertificate>
+              <ButtonCertificate>Detalles del certificado</ButtonCertificate>
+            </BoxCertificate>
+          ))}
         </BoxContain>
       </SectionStylesCertTheme>
     </DataContext.Provider>
