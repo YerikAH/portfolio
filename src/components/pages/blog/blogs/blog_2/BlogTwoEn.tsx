8537b668e3a66484b8e0b487ef9238ea6b41c6f4
Harvey Yerik <yerik05vh@gmail.com>
2023-03-08 22:07:33 -0500

    Dev: update project

--- src/components/pages/blog/blogs/blog_2/BlogTwoEn.tsx
+++ src/components/pages/blog/blogs/blog_2/BlogTwoEn.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
 
 export default function BlogTwoEn() {
-  return <></>
+  return (
+    <>
+      <SpanText>{'//'} Experiencia</SpanText>
+      <HeadlinePrincipal>Mi experiencia aprendiendo programación durante 8 meses</HeadlinePrincipal>
+      <SpanTextTwo>Publicado el 6 de marzo, 2023</SpanTextTwo>
+      <WhoPublished />
+      <ImageBlog src={blogI} />
+    </>
+  )
 }
--- src/components/pages/blog/blogs/blog_2/BlogTwoEs.tsx
+++ src/components/pages/blog/blogs/blog_2/BlogTwoEs.tsx
@@ -1,5 +1,14 @@
 import React from 'react'
 
 export default function BlogTwoEs() {
-  return <></>
+  return (
+    <>
+      {' '}
+      <SpanText>{'//'} Experiencia</SpanText>
+      <HeadlinePrincipal>Mi experiencia aprendiendo programación durante 8 meses</HeadlinePrincipal>
+      <SpanTextTwo>Publicado el 6 de marzo, 2023</SpanTextTwo>
+      <WhoPublished />
+      <ImageBlog src={blogI} />
+    </>
+  )
 }
