4ca989877d665eb1a89f3c2f807f55bf45a30391
Harvey Yerik <yerik05vh@gmail.com>
2023-03-10 21:34:18 -0500

    Dev: update project

--- src/components/pages/project/Main.tsx
+++ src/components/pages/project/Main.tsx
@@ -18,48 +18,52 @@ import GithubComponent from '../../images/GithubComponent'
 import LinkComponent from '../../images/LinkComponent'
 import ThemeContext from '../../../context/themeContext'
 import { useContext } from 'react'
+import DataContext from '../../../context/dataContext'
 export default function Main() {
   const themeContext = useContext(ThemeContext)
+  const dataContext = useContext(DataContext)
   return (
-    <ThemeContext.Provider value={themeContext}>
-      <MainProjectTheme className={themeContext.theme}>
-        <SectionProject>
-          <BoxGridOne>
-            <ImageResponsive>
-              <ImageProject src={aaaw} />
-            </ImageResponsive>
-          </BoxGridOne>
-          <BoxGridTwo>
-            <HeadlinePrincipal>Aplicación de productividad</HeadlinePrincipal>
-            <TextBodyOne>
-              Esta aplicación de productividad cuenta con una funcionalidad de pomodoro para ayudar
-              a aumentar la eficiencia y productividad. Además, permite la adición de tareas para
-              llevar un registro organizado de las tareas pendientes. La aplicación también cuenta
-              con opciones de configuración para personalizarla según las preferencias del usuario.
-              Este proyecto fue un reto significativo en mi camino de aprendizaje de JavaScript
-              Vanilla y estoy muy orgulloso del resultado logrado.
-            </TextBodyOne>
-            <HeadLineTwo>Tecnologías utilizadas en el desarrollo del proyecto</HeadLineTwo>
-            <ListLabelSkills>
-              <ListLabel>HTML</ListLabel>
-              <ListLabel>CSS</ListLabel>
-              <ListLabel>JavaScript</ListLabel>
-              <ListLabel>React</ListLabel>
-            </ListLabelSkills>
-            <HeadLineTwo>Enlaces</HeadLineTwo>
-            <BoxButtons>
-              <ButtonClasic>
-                <GithubComponent />
-                Ver codigo
-              </ButtonClasic>
-              <ButtonClasic>
-                <LinkComponent />
-                Ver aplicación
-              </ButtonClasic>
-            </BoxButtons>
-          </BoxGridTwo>
-        </SectionProject>
-      </MainProjectTheme>
-    </ThemeContext.Provider>
+    <DataContext.Provider value={dataContext}>
+      <ThemeContext.Provider value={themeContext}>
+        <MainProjectTheme className={themeContext.theme}>
+          <SectionProject>
+            <BoxGridOne>
+              <ImageResponsive>
+                <ImageProject src={aaaw} />
+              </ImageResponsive>
+            </BoxGridOne>
+            <BoxGridTwo>
+              <HeadlinePrincipal>Aplicación de productividad</HeadlinePrincipal>
+              <TextBodyOne>
+                Esta aplicación de productividad cuenta con una funcionalidad de pomodoro para
+                ayudar a aumentar la eficiencia y productividad. Además, permite la adición de
+                tareas para llevar un registro organizado de las tareas pendientes. La aplicación
+                también cuenta con opciones de configuración para personalizarla según las
+                preferencias del usuario. Este proyecto fue un reto significativo en mi camino de
+                aprendizaje de JavaScript Vanilla y estoy muy orgulloso del resultado logrado.
+              </TextBodyOne>
+              <HeadLineTwo>Tecnologías utilizadas en el desarrollo del proyecto</HeadLineTwo>
+              <ListLabelSkills>
+                <ListLabel>HTML</ListLabel>
+                <ListLabel>CSS</ListLabel>
+                <ListLabel>JavaScript</ListLabel>
+                <ListLabel>React</ListLabel>
+              </ListLabelSkills>
+              <HeadLineTwo>Enlaces</HeadLineTwo>
+              <BoxButtons>
+                <ButtonClasic>
+                  <GithubComponent />
+                  Ver codigo
+                </ButtonClasic>
+                <ButtonClasic>
+                  <LinkComponent />
+                  Ver aplicación
+                </ButtonClasic>
+              </BoxButtons>
+            </BoxGridTwo>
+          </SectionProject>
+        </MainProjectTheme>
+      </ThemeContext.Provider>
+    </DataContext.Provider>
   )
 }
